ea9f68537edcf422d1b4720137641321e6c48726
nahal <mrym.mhdzde@gmail.com>
2024-08-21 17:51:23 +0330

    finish code about user and admin

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,17 @@
 const express=require('express')
 const router=express.Router();
 const authRouter=require('./auth')
+const userRouter=require('./user')
+const adminRouter=require('./admin')
 
-router.route('/auth',authRouter);
+const error=require('./../middlewares/error')
+
+const {isLoggined,isAdmin}=require('./../middlewares/auth')
+
+router.use('/auth',authRouter);
+router.use('/user',isLoggined,userRouter);
+router.use('/admin',isLoggined,isAdmin,adminRouter);
+
+router.use(error)
 
 module.exports=router;
