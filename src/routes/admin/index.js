702b4f2c81a8195c075dbc0e01353c676befd4f4
nahal <mrym.mhdzde@gmail.com>
2024-08-21 17:30:29 +0330

    create admin route

---
+++ src/routes/admin/index.js
@@ -0,0 +1,13 @@
+const express=require('express')
+const router=express.Router();
+const controller=require('./controller')
+
+router.get(
+  '/',
+  controller.dashboard
+)
+
+
+
+
+module.exports=router;
