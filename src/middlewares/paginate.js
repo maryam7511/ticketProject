84b23a9193f7780a999737faea690608ddbaec62
nahal <mrym.mhdzde@gmail.com>
2024-08-21 17:57:12 +0330

    create paginate page

---
+++ src/middlewares/paginate.js
@@ -0,0 +1,17 @@
+
+const paginate = (req, res, next) => {
+  let data = [];
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 5;
+  const startIndex = (page - 1) * limit;
+  const endIndex = startIndex + limit;
+
+  res.paginatedResults = {
+    results: data.slice(startIndex, endIndex),
+    currentPage: page,
+    totalPages: Math.ceil(data.length / limit),
+  };
+  next();
+};
+
+module.exports= {paginate}
