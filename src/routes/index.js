f72d131756ba781c6063262093c89445bfea353e
nahal <mrym.mhdzde@gmail.com>
2024-08-23 08:30:31 +0330

    createTicketAnswer code

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const authRouter=require('./auth')
 const userRouter=require('./user')
 const adminRouter=require('./admin')
 const ticketRouter=require('./ticket')
+const ticketAnswerRouter=require('./ticketAnswer')
 
 const error=require('./../middlewares/error')
 
@@ -12,6 +13,7 @@ const {isLoggined,isAdmin}=require('./../middlewares/auth')
 router.use('/auth',authRouter);
 router.use('/user',isLoggined,userRouter);
 router.use('/admin',isLoggined,isAdmin,adminRouter);
+router.use('/ticketAnswers',isLoggined,isAdmin,ticketAnswerRouter);
 router.use('/ticket',isLoggined,ticketRouter);
 
 router.use(error)
--- src/routes/ticketAnswer/controller.js
+++ src/routes/ticketAnswer/controller.js
@@ -3,6 +3,7 @@ const _ = require("lodash");
 const { TICKET_ROLE_CODES } = require("./../../models/constants");
 const { Ticket } = require("./../../models/tickets");
 const { User } = require("./../../models/users");
+const { TicketAnswer } = require("./../../models/ticketAnswer");
 
 module.exports = new (class extends controller {
   async getTicketAnswer(req, res) {
@@ -10,7 +11,19 @@ module.exports = new (class extends controller {
   }
 
   async createTicketAnswer(req, res) {
-    
+    const { ticketId, userId, answerText } = req.body;
+    let newTicketAnswer = new Ticket({
+      ticketId,
+      userId:req.user._id,
+      answerText
+       
+    });
+    newTicketAnswer = await newTicketAnswer.save();
+    this.response({
+      res,
+      message: "ok",
+      data: newTicketAnswer,
+    });
   }
 
   async deleteTicketAnswer(req, res) {
